abd33035f3ca34415c9331eb8c8f81b58dfb4bd2
Dmunoz <demuze28@gmail.com>
2018-05-15 02:00:44 -0400

    Finished adding mobile menu functionality. Also added FA icon to the mobile menu bar.

--- functions.php
+++ functions.php
@@ -4,9 +4,8 @@
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package dannydev
+ * 
  */
-
 if ( ! function_exists( 'dannydev_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
@@ -23,10 +22,8 @@ if ( ! function_exists( 'dannydev_setup' ) ) :
 		 * to change 'dannydev' to the name of your theme in all the template files.
 		 */
 		load_theme_textdomain( 'dannydev', get_template_directory() . '/languages' );
-
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
-
 		/*
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
@@ -34,19 +31,16 @@ if ( ! function_exists( 'dannydev_setup' ) ) :
 		 * provide it for us.
 		 */
 		add_theme_support( 'title-tag' );
-
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
 		add_theme_support( 'post-thumbnails' );
-
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'dannydev' ),
 		) );
-
 		/*
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
@@ -58,16 +52,13 @@ if ( ! function_exists( 'dannydev_setup' ) ) :
 			'gallery',
 			'caption',
 		) );
-
 		// Set up the WordPress core custom background feature.
 		add_theme_support( 'custom-background', apply_filters( 'dannydev_custom_background_args', array(
 			'default-color' => 'ffffff',
 			'default-image' => '',
 		) ) );
-
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
-
 		/**
 		 * Add support for core custom logo.
 		 *
@@ -82,7 +73,6 @@ if ( ! function_exists( 'dannydev_setup' ) ) :
 	}
 endif;
 add_action( 'after_setup_theme', 'dannydev_setup' );
-
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
@@ -97,7 +87,6 @@ function dannydev_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'dannydev_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'dannydev_content_width', 0 );
-
 /**
  * Register widget area.
  *
@@ -115,55 +104,44 @@ function dannydev_widgets_init() {
 	) );
 }
 add_action( 'widgets_init', 'dannydev_widgets_init' );
-
 /**
  * Enqueue scripts and styles.
  */
 function dannydev_scripts() {
 	wp_enqueue_style( 'dannydev-style', get_stylesheet_uri() );
-
+	
 	wp_enqueue_script( 'dannydev-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20151215', true );
-	wp_localize_script('dannydev-navigation', 'dannydevScreenReaderText', 	array( 
-
+	
+	wp_localize_script('dannydev-navigation', 'dannydevScreenReaderText', array( 
 				'expand' => __('Expand child menu', 'dannydev'), 
-				'collapse' => __('Collapse child menu', 'dannydev'), 
-
-		)
-);
-
+				'collapse' => __('Collapse child menu', 'dannydev')
+		));
 
 	wp_enqueue_script( 'dannydev-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply', 'dannydevScreenReaderText');
 	}
 }
 add_action( 'wp_enqueue_scripts', 'dannydev_scripts' );
-
 /**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
-
 /**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
-
 /**
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
-
 /**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
 /**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
--- header.php
+++ header.php
@@ -48,6 +48,8 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( '', 'dannydev' ); ?> <i class="fas fa-bars"></i></button>
+
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
